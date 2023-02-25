aaef91aff1bc38c1a07d9a3f9b93726af44f5df9
Edward Li <edward@edward-li.com>
2023-02-25 14:20:10 -0800

    Fixed UI Rendering

--- src/main/ui/TerminalGame.java
+++ src/main/ui/TerminalGame.java
@@ -1,10 +1,14 @@
 package ui;
 
 import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.TerminalSize;
 import com.googlecode.lanterna.TextColor;
 import com.googlecode.lanterna.graphics.TextGraphics;
 import com.googlecode.lanterna.screen.Screen;
+import com.googlecode.lanterna.screen.TerminalScreen;
 import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+import com.googlecode.lanterna.terminal.TerminalFactory;
 import model.Game;
 import model.Tile;
 
@@ -13,15 +17,20 @@ import java.io.IOException;
 public class TerminalGame {
     private Game game;
     private Screen screen;
+    private Terminal terminal;
+    private int tick;
 
     public TerminalGame() {
         // Setup and initialize game object
-        game = new Game(64, 64);
+        game = new Game(32, 32);
 
         // Start the Terminal UI Rendering
         try {
-            // Setup and create screen
-            screen = new DefaultTerminalFactory().createScreen();
+            // Setup and create terminal
+            DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
+            terminalFactory.setInitialTerminalSize(new TerminalSize(40, 40));
+            terminal = terminalFactory.createTerminal();
+            screen = new TerminalScreen(terminal);
             screen.startScreen();
 
             // Start The Game Loop
@@ -39,7 +48,9 @@ public class TerminalGame {
     }
 
     private void startGameLoop() throws IOException, InterruptedException {
+        tick = 0;
         while (true) {
+            tick++;
             // Draw all tiles, entities, and items on the map
             renderGame();
             Thread.sleep(1000L / Game.TPS);
@@ -53,6 +64,8 @@ public class TerminalGame {
 
         // Render all elements
         renderTiles();
+
+        drawSprite(1, 1, Integer.toString(tick).charAt(0), TextColor.ANSI.GREEN, TextColor.ANSI.DEFAULT);
 
         screen.refresh();
     }
